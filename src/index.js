050c5199abf1dec3b9252dd8b90ed975a19c95e6
JuleFloorson <jubo93@web.de>
2020-03-02 17:42:49 +0100

    Add search engine

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './index.scss';
 import { headline } from './components/headline';
+import { search } from './components/search';
 
 document.body.appendChild(headline());
+document.body.appendChild(search());
