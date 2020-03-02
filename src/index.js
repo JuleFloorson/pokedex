32da34f938c55ea4edfb1a5699c8dcbe9329d5fe
JuleFloorson <jubo93@web.de>
2020-03-02 14:12:48 +0100

    Add basic setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'Pokedex';
+
+  return element;
+}
+
+const div = component();
+document.body.appendChild(div);
