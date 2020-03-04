d28fdb05d2e826198cf8b618c58e74c7b3aff9af
Julia Bodensohn <60884165+JuleFloorson@users.noreply.github.com>
2020-03-04 10:03:28 +0100

    Addpokemons (#8)
    * Add pokemon array
    
    * Change font family

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|oft)$/,
+        use: ['file-loader']
       }
     ]
   }
