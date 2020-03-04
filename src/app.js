6d9d2a62411fbd958dca027aa2101bee4b5590d9
JuleFloorson <jubo93@web.de>
2020-03-04 16:06:15 +0100

    Delete comments

--- src/app.js
+++ src/app.js
@@ -1,62 +1,13 @@
 import './app.scss';
 import { createElement } from './lib/dom';
-//import { createHeadline } from './components/headline';
+import { filterPokemons } from './lib/pokemons';
 import { createSearch } from './components/search';
 import pokemonImage from './pictures/pokemonlogo.png';
-// import pokeballImage from './pictures/pokeball.png';
 import { pokemons } from './components/pokemons';
 import { appendContent } from './lib/dom';
 
 //
-const allPokemons = [
-  'Bisasam',
-  'Bisaknosp',
-  'Bisaflor',
-  'Glumanda',
-  'Glutexo',
-  'Glurak',
-  'Schiggi',
-  'Schillok',
-  'Turtok',
-  'Raupy',
-  'Safcon',
-  'Smettbo',
-  'Hornilu',
-  'Korkuna',
-  'Bibor',
-  'Taubsi',
-  'Tauboga',
-  'Tauboss',
-  'Rattfratz',
-  'Rattikarl',
-  'Habitak',
-  'Ibitak',
-  'Rettan',
-  'Arbok',
-  'Pikachu',
-  'Raichu',
-  'Sandan',
-  'Sandamer',
-  'Nidoran',
-  'Nidorina',
-  'Nidoqueen',
-  'Nidoran',
-  'Nidorino',
-  'Nidoking',
-  'Piepi',
-  'Pixi'
-];
 
-//
-function filterPokemons(searchValue) {
-  const lowerCaseSearchValue = searchValue.toLowerCase();
-
-  const filteredPokemons = allPokemons.filter(pokemon => {
-    return pokemon.toLowerCase().startsWith(lowerCaseSearchValue);
-  });
-  return filteredPokemons;
-}
-//
 export function app() {
   //create elements header main container
   const main = createElement('main', { className: 'main' });
@@ -69,26 +20,17 @@ export function app() {
   pokedexContainer.appendChild(pokedexHead);
   pokedexContainer.appendChild(pokedexMain);
 
-  //const headlineElement = createHeadline('Pokedex');
   const searchElement = createSearch(sessionStorage.getItem('searchValue'));
-  //pokedexHead.appendChild(headlineElement);
+
   pokedexMain.appendChild(searchElement);
 
   const pokemImage = createElement('img', {
     className: 'pokeimage',
     src: pokemonImage
   });
-  // const pokebImage = createElement('img', {
-  //   className: 'pokeballimage',
-  //   src: pokeballImage
-  // });
-
-  //
 
   pokedexHead.appendChild(pokemImage);
-  // pokedexHead.appendChild(pokebImage);
-  //ende create elements
-  //
+
   let searchResults = null;
   function setSearchResults() {
     const filteredPokemons = filterPokemons(searchElement.value);
